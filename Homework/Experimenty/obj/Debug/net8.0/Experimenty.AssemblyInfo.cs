ff59d34ab36955a17ef5235816dd0603726db43c
Sebastian <rabotastepa47@mail.ru>
2024-09-11 22:56:38 +0300

    Доработанный калькулятор v3

--- Homework/Experimenty/obj/Debug/net8.0/Experimenty.AssemblyInfo.cs
+++ Homework/Experimenty/obj/Debug/net8.0/Experimenty.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Homework")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Experimenty")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3668417e769c5bf11c3ac0db9cc4c0e3e12dbbf7")]
-[assembly: System.Reflection.AssemblyProductAttribute("Homework")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Homework")]
+[assembly: System.Reflection.AssemblyProductAttribute("Experimenty")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Experimenty")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
--- Homework/Homework/Program.cs
+++ Homework/Homework/Program.cs
@@ -1,91 +1,59 @@
 ﻿using System.ComponentModel.Design;
 
-Console.WriteLine("Напишите первое число + ЭНТЕР и другое + Энтер)");
-//int.TryParse(Console.ReadLine(), out int a);
-//int.TryParse(Console.ReadLine(), out int b);
-//char.TryParse(Console.ReadLine(), out char c);
-
-
-
-if (int.TryParse(Console.ReadLine(), out int a))
+Console.WriteLine("Напишите первое число, затем нажмите Enter)");
+bool numbercheck1 = int.TryParse(Console.ReadLine(), out int FirstValue);
+if (!numbercheck1)
 {
-   //обработка
-}
-else 
-{     
-Console.WriteLine("введён корректный символ!");
-
-    //Ваш текст
-        
-return;
-}
-
-Console.WriteLine("Попробуйте ввести оператор: & | или ^, для проверки системы ошибок");
-
-if (int.TryParse(Console.ReadLine(), out int b))
-{
-    //обработка
-}
-else
-{
-    Console.WriteLine("введён корректный символ!");
-
-    //Ваш текст
-
+    Console.WriteLine("Введён некорректный символ!");
     return;
 }
 
-Console.WriteLine("А теперь знак + или - или * или / для операции подсчёта результата");
-
-if (char.TryParse(Console.ReadLine(), out char c))
+Console.WriteLine("А теперь напишите второе число и нажмите Enter");
+bool numbercheck2 = int.TryParse(Console.ReadLine(), out int SecondValue);
+if (!numbercheck2)
 {
-    //обработка
-}
-else
-{
-    Console.WriteLine("введён корректный символ!");
-
-    //Ваш текст
-
+    Console.WriteLine("Введён некорректный символ!");
     return;
 }
 
+Console.WriteLine("Теперь введите + - / * и Enter");
+bool numbercheck3 = char.TryParse(Console.ReadLine(), out char ThirdValue);
+if (!numbercheck3)
+{
+    Console.WriteLine("Введён некорректный символ!");
+    return;
+}
 
-switch (c)
+int Plus = FirstValue + SecondValue;
+int Minus = FirstValue - SecondValue;
+int Multi = FirstValue * SecondValue;
+int Div = FirstValue / SecondValue;
+
+
+switch (ThirdValue)
 {
     case '+':
-        Console.WriteLine("В десятичной форме:");
-        Console.WriteLine("Result of {0} + {1} = {2}", a, b, a + b);
-        Console.WriteLine ("В шестнадцатиричной форме:");
-        Console.WriteLine(Convert.ToString ( a + b, 16));
-        Console.WriteLine("В двоичной форме:");
-        Console.WriteLine(Convert.ToString(a + b, 2));
+
+        Console.WriteLine("В десятичной форме:" + Plus);
+        Console.WriteLine ("В шестнадцатиричной форме:" + Convert.ToString(Plus, 16));
+        Console.WriteLine("В двоичной форме:" + Convert.ToString(Plus, 2));;
         break;
 
     case '-':
-        Console.WriteLine("В десятичной форме:");
-        Console.WriteLine("Result of {0} - {1} = {2}", a, b, a - b);
-        Console.WriteLine("В шестнадцатиричной форме:");
-        Console.WriteLine(Convert.ToString(a - b, 16));
-        Console.WriteLine("В двоичной форме:");
-        Console.WriteLine(Convert.ToString(a - b, 2));
+        Console.WriteLine("В десятичной форме:" + Minus);
+        Console.WriteLine("В шестнадцатиричной форме:" + Convert.ToString(Minus, 16));
+        Console.WriteLine("В двоичной форме:" + Convert.ToString(Minus, 2));
         break;
 
     case '*':
-        Console.WriteLine("В десятичной форме:");
-        Console.WriteLine("Result of {0} * {1} = {2}", a, b, a * b);
-        Console.WriteLine("В шестнадцатиричной форме:");
-        Console.WriteLine(Convert.ToString(a * b, 16));
-        Console.WriteLine("В двоичной форме:");
-        Console.WriteLine(Convert.ToString(a * b, 2));
+        Console.WriteLine("В десятичной форме:" + Multi);
+        Console.WriteLine("В шестнадцатиричной форме:" + Convert.ToString(Multi, 16));
+        Console.WriteLine("В двоичной форме:" + Convert.ToString(Multi, 2));
         break;
 
     case '/':
-        Console.WriteLine("В десятичной форме:");
-        Console.WriteLine("Result of {0} / {1} = {2}", a, b, a / b);
-        Console.WriteLine("В шестнадцатиричной форме:");
-        Console.WriteLine(Convert.ToString(a / b, 16));
-        Console.WriteLine("В двоичной форме:");
-        Console.WriteLine(Convert.ToString(a / b, 2));
+        Console.WriteLine("В десятичной форме:" + Div);
+        Console.WriteLine("В шестнадцатиричной форме:" + Convert.ToString(Div, 16));
+        Console.WriteLine("В двоичной форме:" + Convert.ToString(Div, 2));
         break;
 }
